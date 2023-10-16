170958914dd03469aa2a041dae560f9b0f8ab55b
ÂmonRê <common.nicolasleclercq@gmail.com>
2023-10-16 13:53:02 +0200

    Downloader et dl de l'historique fdj

---
+++ EuroMillionsAPI.Downloader/Downloader.cs
@@ -0,0 +1,45 @@
+﻿using EuroMillionsAPI.Synchronizer.Services;
+using System.IO.Compression;
+
+namespace EuroMillionsAPI.Synchronizer
+{
+    public class Downloader
+    {
+        private ScrapperServices scrapperServices;
+        private ParserServices parserServices;
+
+        public Downloader()
+        {
+            this.parserServices = new ParserServices();
+            this.scrapperServices = new ScrapperServices();
+        }
+
+        public string DownloadDrawResultFromFdjHistory()
+        {
+            //Get Links
+            List<string> links = parserServices.getDownloadLinksFromFdjHistoryHtmlPage(
+                scrapperServices.getHtmlFromUrl("https://www.fdj.fr/jeux-de-tirage/euromillions-my-million/historique")
+            );
+
+            //Download links
+            var directoryObject = Directory.CreateTempSubdirectory();
+
+            var downloadTasks = new List<Task>();
+            foreach (string link in links)
+            {
+                downloadTasks.Add(scrapperServices.DownloadFileFromTo(link, directoryObject.FullName));
+            }
+            Task.WhenAll(downloadTasks).Wait();
+
+            //Unzip File
+            var archives = Directory.GetFiles(directoryObject.FullName);
+            foreach (string archive in archives)
+            {
+                ZipFile.ExtractToDirectory(archive, directoryObject.FullName);
+                File.Delete(archive);
+            }
+
+            return directoryObject.FullName;
+        }
+    }
+}
---
+++ EuroMillionsAPI.Downloader/Services/ParserServices.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+
+namespace EuroMillionsAPI.Synchronizer.Services
+{
+    public class ParserServices
+    {
+        public ParserServices() { }
+
+        public List<string> getDownloadLinksFromFdjHistoryHtmlPage(HtmlDocument html)
+        {
+            List<string> result = new List<string>();
+
+            HtmlNodeCollection euromillionsAndMyMillionsDiv = html.DocumentNode.SelectNodes("//*[@id=\"page-chart\"]/section[4]/div");
+            IEnumerable<HtmlNode> links = euromillionsAndMyMillionsDiv.Descendants("a");
+            foreach (HtmlNode link in links)
+            {
+                result.Add(link.Attributes["href"].Value);
+            }
+            return result;
+        }
+    }
+}
